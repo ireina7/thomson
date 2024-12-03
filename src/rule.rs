4ac1c9a72ca907a724d3dea963bbbc3640168ff7
ireina7 <comcx.yang@gmail.com>
2024-12-03 17:43:43 +0800

    refactor rule set as trie

--- src/rule.rs
+++ src/rule.rs
@@ -1,4 +1,7 @@
-use std::fmt;
+#![allow(dead_code)]
+use std::{borrow::Borrow, collections::HashMap, fmt, hash::Hash};
+
+use itertools::Itertools;
 
 /// A single rule to combine paths  
 /// `span.len() >= path.len()`
@@ -9,6 +12,13 @@ pub struct Rule {
 }
 
 impl Rule {
+    pub fn empty() -> Self {
+        Self {
+            path: Vec::new(),
+            span: Vec::new(),
+        }
+    }
+
     pub fn len(&self) -> usize {
         self.path.len()
     }
@@ -22,10 +32,10 @@ impl Rule {
     }
 
     pub fn group(&mut self, i: usize, j: usize) {
-        if i > self.span.len() || j > self.span.len() {
+        if i > self.span.len() || j > self.span.len() || j < i {
             return;
         }
-        self.span[i] = j;
+        self.span[i] = j - i;
     }
 
     pub fn extend(&mut self, path: Vec<String>) {
@@ -34,13 +44,17 @@ impl Rule {
         self.span.extend(vec![0; len]);
     }
 
+    pub fn push_front<S: ToString>(&mut self, s: S) {
+        self.extend(vec![s.to_string()]);
+    }
+
     pub fn flatten(&self) -> Vec<String> {
         let mut ans: Vec<String> = vec![];
         let mut i = 0;
         while i < self.path.len() {
             if self.span[i] > 0 {
                 let mut s = String::new();
-                let j = self.span[i];
+                let j = i + self.span[i];
                 while i < self.path.len() && i < j {
                     if !s.is_empty() {
                         s.push_str(".");
@@ -101,6 +115,189 @@ impl fmt::Display for Rule {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct Path<'s>(Vec<Vec<&'s str>>);
+
+impl Default for Path<'_> {
+    fn default() -> Self {
+        Self::empty()
+    }
+}
+
+impl<'s> Path<'s> {
+    pub fn empty() -> Self {
+        Self(Vec::new())
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn push(&mut self, s: &'s str) {
+        self.0.push(vec![s]);
+    }
+
+    fn adhere(&mut self, s: &'s str) {
+        match self.0.last_mut() {
+            None => self.push(s),
+            Some(unit) => unit.push(s),
+        }
+    }
+
+    fn pop(&mut self) -> Option<&'s str> {
+        match self.0.last_mut() {
+            Some(ss) => match ss.pop() {
+                Some(s) => {
+                    if ss.is_empty() {
+                        self.0.pop();
+                    }
+                    Some(s)
+                }
+                None => {
+                    self.0.pop();
+                    self.pop()
+                }
+            },
+            None => None,
+        }
+    }
+
+    pub fn flattern(&self) -> Vec<String> {
+        let mut ans = Vec::new();
+        for ss in &self.0 {
+            let s: String = ss.iter().map(|s| *s).join(".");
+            ans.push(s);
+        }
+        ans
+    }
+
+    fn units(&self) -> Vec<&'s str> {
+        self.0.iter().flatten().map(|s| *s).collect()
+    }
+}
+
+/// Rule set
+#[derive(Debug, Clone)]
+pub struct Rules {
+    root: Box<Node>,
+}
+
+impl Default for Rules {
+    fn default() -> Self {
+        Self {
+            root: Box::new(Node::link(Edge::Restarted)),
+        }
+    }
+}
+
+impl Rules {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn paths(&self) -> Vec<Path<'_>> {
+        let mut collector = Vec::new();
+        let mut path = Path::empty();
+        Self::collect_rules(&self.root, &mut collector, &mut path);
+        collector
+    }
+
+    /// DFS
+    fn collect_rules<'s>(node: &'s Node, collector: &mut Vec<Path<'s>>, path: &mut Path<'s>) {
+        if node.is_leaf() {
+            collector.push(path.clone());
+            return;
+        }
+        for (key, next) in &node.nexts {
+            match next.edge {
+                Edge::Connected => path.adhere(key),
+                Edge::Restarted => path.push(key),
+            }
+            Self::collect_rules(next, collector, path);
+            path.pop();
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub(crate) enum Edge {
+    Connected,
+    Restarted,
+}
+
+impl Default for Edge {
+    fn default() -> Self {
+        Edge::Restarted
+    }
+}
+
+impl Edge {
+    pub fn is_connected(&self) -> bool {
+        matches!(self, Self::Connected)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct Node {
+    edge: Edge,
+    nexts: HashMap<String, Node>,
+}
+
+#[allow(dead_code)]
+impl Node {
+    pub(crate) fn link(edge: Edge) -> Self {
+        Self {
+            edge,
+            nexts: HashMap::new(),
+        }
+    }
+
+    pub(crate) fn insert<S: ?Sized + ToString>(&mut self, edge: Edge, key: &S) -> Option<Node> {
+        self.nexts.insert(key.to_string(), Node::link(edge))
+    }
+
+    pub(crate) fn next<S: ToString>(&mut self, edge: Edge, key: S) -> &mut Node
+    where
+        String: Borrow<S>,
+        S: Hash + Eq,
+    {
+        self.insert(edge, &key);
+        self.get_mut(&key).unwrap() // we know we can safely unwrap here
+    }
+
+    pub(crate) fn compact<S, Iter>(&mut self, path: Iter)
+    where
+        S: ToString + Hash + Eq,
+        String: Borrow<S>,
+        Iter: IntoIterator<Item = (Edge, S)>,
+    {
+        let mut this = self;
+        for (edge, s) in path {
+            this = this.next(edge, s);
+        }
+    }
+
+    pub(crate) fn get<S: ?Sized>(&self, s: &S) -> Option<&Node>
+    where
+        String: Borrow<S>,
+        S: Hash + Eq,
+    {
+        self.nexts.get(s)
+    }
+
+    pub(crate) fn get_mut<S: ?Sized>(&mut self, s: &S) -> Option<&mut Node>
+    where
+        String: Borrow<S>,
+        S: Hash + Eq,
+    {
+        self.nexts.get_mut(s)
+    }
+
+    pub(crate) fn is_leaf(&self) -> bool {
+        self.nexts.is_empty()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -124,5 +321,22 @@ mod test {
         let rule = rule.join(that);
 
         dbg!(format!("{}", rule));
+    }
+
+    #[test]
+    fn test_rules_set() {
+        let mut rules = Rules::new();
+        rules.root.insert(Edge::Restarted, "editor");
+        rules.root.insert(Edge::Restarted, "window");
+
+        let editor = rules.root.get_mut("editor").unwrap();
+        editor.insert(Edge::Connected, "font");
+        editor.insert(Edge::Connected, "size");
+        editor.insert(Edge::Restarted, "restarted");
+
+        let paths = rules.paths();
+        for path in paths {
+            dbg!(path.flattern());
+        }
     }
 }
